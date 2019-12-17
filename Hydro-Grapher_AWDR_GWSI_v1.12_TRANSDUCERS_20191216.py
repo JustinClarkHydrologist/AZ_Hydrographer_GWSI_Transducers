42231bda4e1867843759ddeb617dc1e02af2a352
Michael T. Giansiracusa <giansiracumt@ornl.gov>
2019-12-17 12:44:04 -0500

    Basic cleanup. I added a flag so we don't have to auto view the plots and moved some notes to another file.

--- Hydro-Grapher_AWDR_GWSI_v1.12_TRANSDUCERS_20191216.py
+++ Hydro-Grapher_AWDR_GWSI_v1.12_TRANSDUCERS_20191216.py
@@ -26,10 +26,11 @@ Approximate Run Time = 59 minutes       (HP Z240 Tower Workstation
 """
 import pandas as pd
 import matplotlib.pyplot as plt
-import datetime  # TODO this is unused and should be removed in a cleanup branch
 from matplotlib import dates
 from multiprocessing import Pool, cpu_count
 
+# show plots or not?
+SHOW = False
 
 ##MAKE VARIABLES FOR THE FILENAMES
 filename1 = "GWSI_TRANSDUCER_LEVELS.txt"
@@ -132,7 +133,8 @@ def figure_proc(location):
 
     plt.rcParams.update({'font.size': 12})
 
-    plt.show() # TODO remove this, it significantly slows down the program.
+    if SHOW:
+        plt.show() # TODO remove this, it significantly slows down the program.
 
     outname = str('Hydrographs_GWSI_Automated__') + str(location) + str('.png')
     fig.savefig(outname, dpi=400, bbox_inches='tight', pad_inches=.1)
@@ -142,36 +144,4 @@ with Pool(cpu_count()) as pool:
     pool.map(figure_proc, wells)
     pool.join()
 
-
-#    outname = str('GWSI_WLE_ZipExtract_Seasonal_') + str(location) + str('__Raw_Data_Table.csv')
-#    df4.to_csv(outname, index=False)
-#
-#    outname = str('_Error_Date_Bad.csv')
-#    df.to_csv(outname, index=False)
-
-##############################################################################
-##############################################################################
-##############################################################################
-#  ### ### ### ### ### #### ### ### ### ### ### #### ### ### ### ### ### ###  #
-##   Example and Test Code Used for This Program   ##
-"""
-outname = str('test_df4.csv')
-df4.to_csv(outname, index=False)
-
-
-###seasonal trending wells with high variation:
-#wells = [315614110543601, 315705110560901, 315909110540601, 320216110405901, 320249110413501, 320944111125701, 321034111140801, 321058110563301, 321244110504401, 321348111154401, 321404111153201, 321414111133601, 321424110432901, 321445110554601, 321456111160201, 321503110484901, 321504110521601, 321512110480701, 321523110491801, 321530110523501, 321537110585001, 321542110502701, 321548110561801, 321601110544601, 321703110575401, 321706110442201, 321745111012801, 321748110582101, 322238111065701, 322339111170001, 323635111330001]
-
-###SET GRAPH STYLE, FORMATTING
-#import seaborn as sns
-#sns.set_style("darkgrid")
-
-"""
-###############################################################################
-#  ### ### ### ### ### #### ### ### ### ### ### #### ### ### ### ### ### ###  #
-##   Websites Visited   ##
-"""
-secondary x axis:
-https://pythonmatplotlibtips.blogspot.com/2018/01/add-second-x-axis-below-first-x-axis-python-matplotlib-pyplot.html
-"""
 
